4319375765fa5b1a82521f3d769c6814a9d07633
Subhamsekhar Panda <Subhamsekhar.Panda@citiustech.com>
2024-06-11 18:17:45 +0530

    Modular Project Structure Created

---
+++ Patient-Integration-Project/routes/inbound/patient-inbound-file-in/src/main/java/com/citiustech/inbound/FiletoXslateRoute.java
@@ -0,0 +1,80 @@
+package com.citiustech.inbound;
+
+
+import java.io.FileNotFoundException;
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import org.apache.activemq.ConnectionFailedException;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.common.HttpOperationFailedException;
+
+public class FiletoXslateRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+		//Exception Handling
+				//Http Invocation Exception
+				onException(HttpOperationFailedException.class)
+				.handled(true)
+				.log("Http Request Failed : ${exception.message}")
+				.maximumRedeliveries(3)
+				.retryAttemptedLogLevel(LoggingLevel.WARN);
+				
+				//ActiveMQ connection Exception
+				onException(ConnectionFailedException.class)
+				.handled(true)
+				.log("ActiveMQ Connection Failed : ${exception.message}")
+				.maximumRedeliveries(3)
+				.maximumRedeliveryDelay("1000")
+				.retryAttemptedLogLevel(LoggingLevel.WARN);
+				
+				//File Not Found Exception
+				onException(FileNotFoundException.class)
+				.handled(true)
+				.log("File Not Exist : ${exception.message}")
+				.retryAttemptedLogLevel(LoggingLevel.WARN);
+				
+				//Rest Api Exception
+				onException(ConnectException.class)
+				.handled(true)
+				.log("Rest Api Call Failed : ${exception.message}")
+				.maximumRedeliveries(3)
+				.maximumRedeliveryDelay("1000")
+				.retryAttemptedLogLevel(LoggingLevel.WARN);
+				
+				//SQL Exception 
+				onException(SQLException.class)
+				.handled(true)
+				.log("SQL Exception occurred: ${exception.message}");		
+						
+				//Default Error Handler
+				onException(Exception.class)
+				.handled(true)
+				.log("Exception occurred: ${exception.message}");				
+
+		from("file:output?fileName=PatientIds.txt&noop=true")
+		.routeId("filetodirect")
+		.split(body().tokenize("\n"))
+		.setBody(simple("${body.trim()}"))
+		.log("${body}")
+		.choice()
+			.when(body().isEqualTo(""))
+			.log("Empty Line in File")
+		.otherwise()
+			.to("direct:PatientIdsfile");
+		
+		from("direct:PatientIdsfile")
+		.routeId("directToHttpAndQueue")
+		.removeHeaders("*")
+		.setHeader(Exchange.HTTP_METHOD,simple("GET"))
+		.setHeader(Exchange.HTTP_URI,simple("http://localhost:8080/PatientData/${body}"))
+		.to("http://localhost:8080/PatientData/${body}")
+		.to("activemq:queue:patient-xslate");
+
+	}
+
+}
---
+++ Patient-Integration-Project/routes/xslate/patient-xslate-json/src/main/java/com/citiustech/xslate/XslateRouteBuilder.java
@@ -0,0 +1,145 @@
+package com.citiustech.xslate;
+
+import org.apache.activemq.ConnectionFailedException;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+import com.citiustech.models.DiagnosisDetails;
+import com.citiustech.models.EquipmentDetails;
+import com.citiustech.models.NurseDetails;
+import com.citiustech.models.Patient;
+import com.citiustech.models.PatientDemographicDetails;
+import com.citiustech.models.PatientTreatmentDetails;
+import com.citiustech.models.TransformedPatient;
+
+public class XslateRouteBuilder extends RouteBuilder {
+	
+	private String xslateJsonStructure;
+
+	public String getXslateJsonStructure() {
+		return xslateJsonStructure;
+	}
+
+	public void setXslateJsonStructure(String xslateJsonStructure) {
+		this.xslateJsonStructure = xslateJsonStructure;
+	}
+
+	@Override
+	public void configure() throws Exception {
+		
+		onException(ConnectionFailedException.class)
+		.handled(true)
+		.log("ActiveMQ Connection Failed : ${exception.message}")
+		.maximumRedeliveries(3)
+		.maximumRedeliveryDelay("1000")
+		.retryAttemptedLogLevel(LoggingLevel.ERROR);
+		
+		//Default Error Handler
+		onException(Exception.class)
+		.handled(true)
+		.log("Exception occurred: ${exception.message}");			
+
+		from("activemq:queue:patient-xslate")
+		.unmarshal().json(JsonLibrary.Jackson,Patient.class)
+		.log("${body}")
+		.process(e->{
+			Patient patient = e.getIn().getBody(Patient.class);
+			
+			TransformedPatient patientData = new TransformedPatient();
+
+			PatientDemographicDetails patientDemographic = new PatientDemographicDetails();
+			patientDemographic.setPatientId(patient.getPatientId());
+			patientDemographic.setPatientFirstName(patient.getPatientFirstName());
+			patientDemographic.setPatientLastName(patient.getPatientLastName());
+			patientDemographic.setPatientAge(patient.getPatientAge());
+			patientDemographic.setPatientGender(patient.getPatientGender());
+
+			PatientTreatmentDetails patientTreatmentDetails = new PatientTreatmentDetails();
+
+			DiagnosisDetails diagnosisDetails = new DiagnosisDetails();
+			diagnosisDetails.setDiagnosisDisease(patient.getDiagnosisDisease());
+			diagnosisDetails.setDiagnosisSymptoms(patient.getDiagnosisSymptoms());
+			diagnosisDetails.setDiagnosisMedication(patient.getDiagnosisMedication());
+			diagnosisDetails.setPatientStatus(patient.getPatientStatus());
+
+			EquipmentDetails equipmentDetails = new EquipmentDetails();
+			equipmentDetails.setEquipmentStatus(patient.getEquipmentStatus());
+			equipmentDetails.setEquipmentName(patient.getEquipmentName());
+
+			NurseDetails nurseDetails = new NurseDetails();
+			nurseDetails.setNurseId(patient.getNurseId());
+			nurseDetails.setNurseName(patient.getNurseName());
+
+			patientTreatmentDetails.setDiagnosisDetails(diagnosisDetails);
+			patientTreatmentDetails.setEquipmentDetails(equipmentDetails);
+			patientTreatmentDetails.setNurseDetails(nurseDetails);
+
+			patientData.setPatientDemographicDetails(patientDemographic);
+			patientData.setPatientTreatmentDetails(patientTreatmentDetails);
+			e.getIn().setBody(patientData);
+			
+		})
+		.marshal().json(JsonLibrary.Jackson)
+		.to("activemq:queue:Outbound")
+		.log("Data Sent to Outbound Queue");
+
+//		Method 3
+//		from("activemq:queue:patient-xslate")
+//		.log("Data Recieved From Inbound: ${body}")
+//		.unmarshal().json(JsonLibrary.Jackson)
+//		.setBody(simple(getXslateJsonStructure()))
+//		.to("activemq:queue:Outbound")
+//		.log("Data Sent to Outbound Queue");
+
+		
+		//Method-1
+//		.process(exchange -> {
+//			String originalData = exchange.getIn().getBody(String.class);
+//			ObjectMapper objectmapper = new ObjectMapper();
+//			JsonNode originalJson = objectmapper.readTree(originalData);	
+//			JsonNode transformedJson = ((ObjectNode) objectmapper.createObjectNode().set("Patient_Demographic_Details",
+//					objectmapper.createObjectNode().put("id", originalJson.get("id").asInt())
+//							.put("name", originalJson.get("name").asText()).put("dob", originalJson.get("dob").asText())))
+//					.set("Patient_Treatment_Details", ((ObjectNode) ((ObjectNode) objectmapper.createObjectNode()
+//							.set("diagnosisdetails",objectmapper.createObjectNode().put("diagnosis", originalJson.get("diagnosis").asText())))
+//							.set("equipment details",objectmapper.createObjectNode().put("equipment", originalJson.get("equipment").asText())))
+//							.set("nursedetails", objectmapper.createObjectNode().put("nurseid", originalJson.get("nurseId").asText()).put("name", originalJson.get("nurseName").asText())));
+//			String transformedJsonString= objectmapper.writeValueAsString(transformedJson);
+//			exchange.getIn().setBody(transformedJsonString);
+//		})	
+
+		//Method 2 By using a LinkedHashMap
+
+		//Method 3
+//		.setBody(simple("{" +
+//			    "\"PatientDemographicDetails\": {" +
+//			        "\"PatientId\": \"${body[PatientId]}\"," +
+//			        "\"PatientFirstName\": \"${body[PatientFirstName]}\"," +
+//			        "\"PatientLastName\": \"${body[PatientLastName]}\"," +
+//			        "\"PatientAge\": ${body[PatientAge]},"+
+//			        "\"PatientGender\": \"${body[PatientGender]}\""+
+//			    "}," +
+//			    "\"PatientTreatmentDetails\": {" +
+//			        "\"DiagnosisDetails\": {" +
+//			            "\"DiagnosisDisease\": \"${body[DiagnosisDisease]}\"," +
+//			            "\"DiagnosisSymptoms\": \"${body[DiagnosisSymptoms]}\"," +
+//			            "\"DiagnosisMedication\": \"${body[DiagnosisMedication]}\"," +
+//			            "\"PatientStatus\": \"${body[PatientStatus]}\""+
+//			            
+//			        "}," +
+//			        "\"Equipment\": {" +
+//			            "\"EquipmentName\": \"${body[EquipmentName]}\"," +
+//			            "\"EquipmentStatus\": \"${body[EquipmentStatus]}\"" +
+//			        	
+//			        "}," +
+//			        "\"nursedetails\": {" +
+//			            "\"NurseId\": \"${body[NurseId]}\"," +
+//			            "\"NurseName\": \"${body[NurseName]}\"" +
+//			        "}" +
+//			    "}" +
+//			"}"))
+//			.log("${body}")
+//			.to("activemq:queue:outbound");
+	}
+}
